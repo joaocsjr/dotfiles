28224f613007a499065c9be72ab2072f7722fa33
jcastro@heineken <jcastro@heineken>
2022-03-01 10:56:55 -0300

    ansible-net

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@ sudo apt update -y
 sudo apt install ansible git -y
 git clone https://github.com/joaocsjr/dotfiles.git
 cd dotfiles 
-ansible-playbook workstation.yml -vvv
+ansible-playbook workstation.yml -v
