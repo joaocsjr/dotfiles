6a6fd4a7e379792a5b30821cd69ef63a6f5921f4
João Castro <jcastro@jcastro.net>
2022-03-21 15:49:56 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
 #!/bin/bash 
 
-sudo apt update -y
-sudo apt install ansible git -y
+#sudo apt update -y
+#sudo apt install ansible git -y
+sudo dnf install ansible git -y 
+
+
 git clone https://github.com/joaocsjr/dotfiles.git
 cd dotfiles 
 ansible-playbook workstation.yml -v
